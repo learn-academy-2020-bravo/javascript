95e84c2f678f47b8367674c8009749cdea785a1b
sjproctor <sproctor950@gmail.com>
2020-04-28 15:07:33 -0700

    adds javascript conditionals functionaslity

---
+++ conditionals-sarah-rachael.js
@@ -0,0 +1,53 @@
+// Here is a JS file that is named for the project and for the team.
+
+// #### Step 1
+// - The Driver should navigate to an appropriate location on your machine
+// - Clone this directory
+// - $ git clone filepath
+// - cd into the directory
+// - Create a new JS file using the command "touch"
+// - Name the file after your pair or team (for example: sarah-rachael.md)
+// - Remember, no spaces in the file name!
+// - Open the directory into a text editor
+// - Open the file you created and add some content - whatever you want
+// - View you markdown with control-shift-m
+// - Save the file
+// - In terminal, ensure you are in the correct directory
+// - $ git pull origin master
+// - $ git add .
+// - $ git commit -m "our first commit"
+// - $ git push origin master
+// - Switch roles
+//
+// #### Step 2
+// - The new Driver should navigate to an appropriate location on your machine
+// - Clone this directory
+// - cd into the directory
+// - Open the directory into a text editor
+// - Make changes to the markdown file
+// - In terminal, ensure you are in the correct directory
+// - $ git pull origin master
+// - $ git add .
+// - $ git commit -m "description of changes"
+// - $ git push origin master
+// - Switch roles
+//
+// ### Step 3
+// - The new Driver should ensure they are in the correct directory
+// - $ git pull origin master
+// - Make changes to the markdown file
+// - In terminal, ensure you are in the correct directory
+// - $ git pull origin master
+// - $ git add .
+// - $ git commit -m "description of changes"
+// - $ git push origin master
+// - Switch roles
+//
+// ### Step 4
+// - Repeat Step 3
+//
+// ### Step 5
+// - Repeat Step 3
+//
+// ### Step 6
+// - Repeat Step 3
