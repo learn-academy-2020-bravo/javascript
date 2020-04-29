5b9577b44094c3f19f5c0fd5c5e962a4392edd7d
Austin Walker <austinsrwalker@gmail.com>
2020-04-29 10:42:09 -0700

    arryas2

--- arraysof-jazyjaz-austin.js
+++ arraysof-jazyjaz-austin.js
@@ -1,7 +1,21 @@
-// lets make some arrays
-//Consider this variable:
-//Write the code that will add "soda" to the end of the original array.
-//Write the code that will add "granola" to the end of array without altering the original array.
-var groceryList = ["chips", "dip", "cookies"]
-var groceryAdd = groceryList.push("soda")
-console.log(groceryList);
+// // lets make some arrays
+// //Consider this variable:
+// //Write the code that will add "soda" to the end of the original array.
+// //Write the code that will add "granola" to the end of array without altering the original array.
+// // var groceryList = ["chips", "dip", "cookies"]
+// // var groceryAdd = groceryList.push("soda")
+// // console.log(groceryList);
+//
+// // Write the code that will add the number 0 to the beginning of the array.
+// //
+// // Write the code that will add the number 12 to the end of the array.
+// //
+// // Write the code that will remove the first number from the array.
+// //
+// // Write the code that will add the number 0 to the beginning of the array without altering the original array.
+// //
+// var numbers = [2, 4, 6, 8, 10]
+// var numberAdd = numbers.unshift("0")
+// var numberEnd = numbers.push("12")
+// var numberShift = numbers.shift()
+// console.log(numbers);
