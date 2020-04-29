96a1e32808ab5c6f2db6fe4e6dda2d4d2c799c8e
April Rieger <april.rieger@yahoo.com>
2020-04-29 10:33:46 -0700

    April's commiting challenges 3&4

--- arrays-april-jeremy-ruben.js
+++ arrays-april-jeremy-ruben.js
@@ -9,11 +9,51 @@
 
 
 
-var number  = [2, 4, 6, 8, 10]
-var zero = [0]
-number.unshift(0)
-number.push(12)
-number.shift()
-var newArray = zero.concat(number)
-console.log(newArray)
+// var number  = [2, 4, 6, 8, 10]
+// var zero = [0]
+// number.unshift(0)
+// number.push(12)
+// number.shift()
+// var newArray = zero.concat(number)
+// console.log(newArray)
 
+// Write the code that finds the index of the first appearance of the number 2.
+// var numSet = [2, 13, 6, 8, 4, 2]
+//
+// console.log(numSet.indexOf(2))
+
+// Write the code that finds the index of the last appearance of the number 2.
+
+// var numSet = [2, 13, 6, 8, 4, 2]
+//
+// console.log(numSet.lastIndexOf(2))
+
+
+
+// Write the code that brings all the letters in the chars array together into a string.
+
+// var chars = ["y", "a", "r", "r", "a"]
+//
+// console.log(chars.join(""))
+
+// Write the code that reverses the order of the letters in the chars array and saves it into a variable called charsReversed.
+
+// var chars = ["y", "a", "r", "r", "a"]
+// var charsReversed = chars.reverse()
+//
+// console.log(charsReversed)
+
+
+// Write the code that brings all the letters in the charsReversed array together into a string with a hyphen between each letter.
+
+// var chars = ["y", "a", "r", "r", "a"]
+// var charsReversed = chars.reverse()
+//
+// console.log(charsReversed.join("-"))
+
+// Write the code that brings all the letters in the charsReversed Array together into a string without separators.
+
+// var chars = ["y", "a", "r", "r", "a"]
+// var charsReversed = chars.reverse()
+//
+// console.log(charsReversed.join(""))
