a61421e5872cf813ba9c07c177140378b4a20278
Ara Jo <arajodeveloper@gmail.com>
2020-04-29 11:51:53 -0700

    easy

--- loops-nikki-ara.js
+++ loops-nikki-ara.js
@@ -16,3 +16,26 @@
 //     console.log(i)
 //   }
 // }
+var nums = [3, 57, -9, 20, 67]
+var highest = 0
+
+for(let i=0; i < nums.length; i++) {
+  if(nums[i] > highest) {
+      highest = nums[i]
+  }
+}
+// console.log(highest)
+
+var nums = [3, 57, -9, 20, 67]
+var lowest = 0
+
+for(let i=0; i < nums.length; i++) {
+  if(nums[i] < lowest) {
+      lowest = nums[i]
+  }
+}
+// console.log(lowest)
+// var remainder = nums[i] % 2
+for(let i=0; i < nums.length; i++) {
+  console.log(nums[i]%2);
+}
