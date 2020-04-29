c0ac4936672e45031d966c31eb3ed72d9bc5ade4
April Rieger <april.rieger@yahoo.com>
2020-04-29 12:12:55 -0700

    April's commit of loops section 2 question 1-2

--- loops-april-jeremy-ruben.js
+++ loops-april-jeremy-ruben.js
@@ -6,12 +6,34 @@
 //         console.log(i * 3)
 //     }
 
-    for(let i = 1; i <= 20; i++) {
-        if(i % 2 === 0){
-            console.log(i);
-        } else {
-            console.log("ODD");
-            
-        }
+    // for(let i = 1; i <= 20; i++) {
+    //     if(i % 2 === 0){
+    //         console.log(i);
+    //     } else {
+    //         console.log("ODD");
+    //     }
+    // }
 
-    }
+    // Create a loop that will log the highest number from the array. Expected output --> 67
+
+
+
+var nums = [3, 57, -9, 20, 67]
+var sort = nums.sort((a,b) => a-b).reverse()
+
+for (let i=0; i < nums.length; i++){
+  if(nums[i] === 67){
+    console.log(nums[i])
+  }
+}
+
+
+
+
+// Create a loop that will log the lowest number from the array Expected output --> -9
+//
+//   var nums = [3, 57, -9, 20, 67]
+//
+// // Create a loop that will log the remainder of each number when divided by 2. Expected output --> 1, 1, -1, 0, 1
+//
+//   var nums = [3, 57, -9, 20, 67]
