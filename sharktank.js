c3f4b01fb838a08438f8551fe4b32315b6375dbf
Austin Walker <austinsrwalker@gmail.com>
2020-04-29 14:48:58 -0700

    jaz take the wheel

--- sharktank.js
+++ sharktank.js
@@ -1,1 +1,55 @@
-cool cool
+// Write a function named marco that returns "polo".
+//
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+//
+// const greeting = (name) => {
+//   return `Welcome ${name} here`
+// }
+//
+// console.log(greeting("someone"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+//
+// declare function named oddOrEven with 2 variables
+var oddOrEven = (numbersGoHere) => {
+var getMod = (numbersGoHere%2)
+  // first argument modulos the number by 2
+
+  if (getMod === 1){
+//if 1 retrun "odd"
+    return "odd"}
+
+  else if (getMod === 0){
+// if 0 return "even"
+    return "even"}
+
+  else {
+// else "the system is broken"
+    return "broken"}
+}
+
+console.log(oddOrEven(9));
+
+
+
+
+
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+//
+// Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
+//
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+//
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+//
+// delcare a constant named "marco"
+
+  // Have Marco return "polo" when called
+
+  // const marco = () => {
+  //   return "Polo"
+  // }
+  //
+  // console.log(marco())
+// call Marco
