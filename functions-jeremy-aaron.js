3c9e48bbb86fde30b08bfa239b773a3aff6b1f46
aaron masanes <aaronmasanes@hotmail.com>
2020-04-29 14:54:05 -0700

    updated 3pm

--- functions-jeremy-aaron.js
+++ functions-jeremy-aaron.js
@@ -34,3 +34,82 @@
 // // Declare function that takes two numbers
 // // Returns result of one number mulitiplied by the other
 // const multiply = (num1, num2) => num1 * num2
+
+
+//write a function called takes in two parameters
+// return remainder if it equals zero
+
+// const divisibleBy = (num1, num2) => {
+//   if (num1 % num2 === 0) {
+//     return `${num1} is evenly divislbe by ${num2}`
+//   }
+//   else {
+//     return "not divisible"
+//   }
+// }
+//
+// console.log(divisibleBy(15, 2))
+
+//create a function called assignGrade
+//which takes in a score and returns a letter assignGrade
+//
+// const assignGrade = (score) => {
+//   let grade = "";
+//   if (score >= 90) {
+//     return "A"
+//   }
+//   else if (score >=80) {
+//     return  "B"
+//   }
+// }
+
+// console.log(assignGrade(85));
+
+//create a function greatNum that takes two parameters
+//return the bigger num
+
+//console.log(greatNum(10,9));
+
+//write a function helloWord takes one argument
+//create a conditional if "es" return "hola"
+//returns "hello World"
+
+// const helloWorld = (hi) => {
+//   if (hi === "es") {
+//     return "hola mundo"
+//   }
+//   else if (hi === "hi") {
+//     return "hallo welt";
+//   }
+//   else {
+//     return "hello world";
+//   }
+// }
+//
+// console.log(helloWorld('es'));
+// console.log(helloWorld('hi'));
+// console.log(helloWorld("sefrjbesrf"))
+
+//write a function called pluralizer takes to argument
+//takea  num and singular noun
+//concat s to noun
+
+const pluralizer = (num, noun) => {
+  if (num === 1) {
+    return `1 ${noun}`
+  }
+  else if (noun === 'sheep') {
+    return `${num} ${noun}`
+  }
+  else if (noun === "child") {
+    noun = 'children'
+    return `${num} ${noun}`
+  }
+  else {
+    return `${num} ${noun}s`
+  }
+}
+console.log(pluralizer(10, "sheep"));
+console.log(pluralizer(20, 'child'));
+// console.log(pluralizer(5, "cat"));
+// console.log(pluralizer(1, "dog"));
--- loops-aaron-chrissie.js
+++ loops-aaron-chrissie.js
@@ -6,29 +6,30 @@
 //   }
 //   }
 
-//var nums = [3, 57, 9, 20, -67, 79, 20];
-// var lowestVal = nums[0];
-// for (let i = 0; i < nums.length; i++) {
-//   if (nums[i] <= lowestVal) {
-//   lowestVal = nums[i];
-//     console.log(nums[i])
-//   }
-//   //console.log(lowestVal)
-//   }
+var nums = [3, 57, 9, 20, -67, 79, 20];
+var lowestVal = nums[0];
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] >= lowestVal) {
+  lowestVal = nums[i];
+    //console.log(nums[i])
+  }
+  //console.log(lowestVal)
+  }
+  console.log(lowestVal)
 
 // var nums = [3, 57, -9, 20, 67];
 // for (let i = 0; i < nums.length; i++) {
 //     console.log(nums[i] % 2)
 // }
 
-var myString = "learn students";
-let newString = myString.split("");
-let count = 0;
-//console.log(newString)
-for (i = 0; i < newString.length; i++) {
-if (newString[i] === 'e') {
-  count++
-}
-//console.log(count)
-}
-console.log(count)
+// var myString = "learn students";
+// let newString = myString.split("");
+// let count = 0;
+// //console.log(newString)
+// for (i = 0; i < newString.length; i++) {
+// if (newString[i] === 'e') {
+//   count++
+// }
+// //console.log(count)
+// }
+// console.log(count)
