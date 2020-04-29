605cf87e86542cb8c6c2257089f8ca4fd57a5382
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-04-29 14:23:08 -0700

    First 5 questions

---
+++ functions-jeremy-aaron.js
@@ -0,0 +1,36 @@
+
+// // Declare a function named marco that returns "polo"
+// const marco = () => {
+//   // Returns "polo"
+//   return "polo"
+// }
+
+
+
+// // Decare a function named greeting taking name as arg
+// const greeting = (name) => {
+//   // Returns "Welcome" message
+//   return `Welcome, ${name}!`
+// }
+
+//
+// // Declare function taking numer as arg
+// const oddOrEven = (number) => {
+//   // conditional if even number
+//   if (number % 2 === 0) {
+//     // Returns whether number is odd or even
+//     return "Even!"
+//     // All else is odd
+//   } else {
+//     return "Odd!"
+//   }
+// }
+
+// // Declare function taking number as arg
+// // Returns number times three
+// const triple = number => number * 3
+
+
+// // Declare function that takes two numbers
+// // Returns result of one number mulitiplied by the other
+// const multiply = (num1, num2) => num1 * num2
