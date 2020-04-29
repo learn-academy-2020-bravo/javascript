b974ebbf170a322bf97bfa6f8e0bc8d974c73b3d
nbrooks89 <nbrooks2389@gmail.com>
2020-04-29 14:32:28 -0700

    new function

---
+++ functions-nikki-april.js
@@ -0,0 +1,15 @@
+//declare a function called marco it does not require an argument
+// const marco = () => {
+//return the value polo
+//   return "polo"
+// }
+// console.log(marco())
+
+//declare a function named greeting and it takes an argument name
+//var name = "NIKKI"
+const greeting = (name) => {
+
+  //returns "Welcome, <person's name here>!"
+  return `Welcome, ${name}!`
+}
+console.log(greeting("nikki"))
