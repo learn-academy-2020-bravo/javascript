a8d09dbaad61cd4a293285890db8d85494f80dc4
Austin Walker <austinsrwalker@gmail.com>
2020-04-29 15:51:28 -0700

    here you go

--- sharktank.js
+++ sharktank.js
@@ -33,18 +33,29 @@
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
 
 //declare a variable function named trippleMe
-var trippleMe = (numbGoHere) =>{
-//declare variable numbGoHere and multiply it by 3
-var trippled = (numbGoHere*3)
-//return tripleMe is numbersGoHere times 3
-  return (trippled)
-}
-//call function trippleMe
-console.log(trippleMe(2));
+// var trippleMe = (numbGoHere) =>{
+// //declare variable numbGoHere and multiply it by 3
+// var trippled = (numbGoHere*3)
+// //return tripleMe is numbersGoHere times 3
+//   return (trippled)
+// }
+// //call function trippleMe
+// console.log(trippleMe(2));
 
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
-//
+  // declare a constant named multiply
+  const multiplier = (num1, num2) => {
+    //  return <nums> with a number multipled by the other
+    return (num1 * num2)
+  }
+  console.log(multiplier (3, 5))
+    //  an argument of <nums>
+    // call the function
+
+
+
+
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 //
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
