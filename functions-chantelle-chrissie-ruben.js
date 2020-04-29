df47a0831d4b4e06c5897164b5014c9698dcedf4
LEARN Academy <cakau9@gmail.com>
2020-04-29 14:58:35 -0700

    Chrissie passing it off

--- functions-chantelle-chrissie-ruben.js
+++ functions-chantelle-chrissie-ruben.js
@@ -1,41 +1,80 @@
-// Write a function named marco that returns "polo".
+// // Write a function named marco that returns "polo".
 
-// const marco = () => {
-//     return "polo"
+// // const marco = () => {
+// //     return "polo"
 
+// // }
+
+// // console.log(marco())
+
+// // Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// // const greeting = (name) => {
+// //     return `Welcome, ${name}!`
+// // }
+
+// // console.log(greeting("Chrissie"));
+
+
+// // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+// const oddOrEven = (num) => {
+//     // if else statement to determine even or odd
+//     if(num % 2 === 1 ) {
+//         return "The number is ODD!"
+//     } else if (num % 2 === 0) {
+//         return "The number is EVEN!"
+//     } else {
+//         return "ERROR!"
+//     }
 // }
 
-// console.log(marco())
+// console.log(oddOrEven("mayonaisse"));
 
-// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
 
-// const greeting = (name) => {
-//     return `Welcome, ${name}!`
+// // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// //declare function that takes a number as an argument
+// const triple = (num) => {
+// //return num mutliplied by 3
+//     return num*3
 // }
+// //console logging three should return 9
+// console.log(triple(3))
+// //console log should return 3
+// console.log(triple(1))
 
-// console.log(greeting("Chrissie"));
-
-
-// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
-
-const oddOrEven = (num) => {
-    // if else statement to determine even or odd
-    if(num % 2 === 1 ) {
-        return "The number is ODD!"
-    } else if (num % 2 === 0) {
-        return "The number is EVEN!"
-    } else {
-        return "ERROR!"
-    }
-}
-
-console.log(oddOrEven("mayonaisse"));
-
-
-// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
 
 // Write a function named multiply that takes two numbers as arguments and returns the result of one of the numbers multiplied by the other.
 
+// //declare a function named multiply that takes two numbers as arguments
+// const multiply = (num1, num2) => {
+// //return would be the result of num1 multiplied by num2
+//     return num1*num2
+// }
+// //console log would return 6
+// console.log(multiply(3, 2))
+// //console log would reutrn 120
+// console.log(multiply(10,12))
+
+
+
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// const divisibleBy = (num1, num2) => {
+// //if and else statement to see if num 1 is evenly divisible by num2
+//     if(num1 % num2 === 0){
+//         return `${num1} is evenly divisible by ${num2}`
+//     } else if(num1 % num2 >= 1 || num1 % num2 < 0){
+//         return `${num1} is not evenly divisible by ${num2} indubitibly`
+//     } else {
+//         return "Uh oh error!"
+//     }
+    
+// }
+// console.log(divisibleBy(10, 2))
+// console.log(divisibleBy(84, 17))
+// console.log(divisibleBy("peanut butter", "jelly"))
+// console.log(divisibleBy(-24, 7))
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
