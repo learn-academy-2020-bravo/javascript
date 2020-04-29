c25fa54e20c1c791c55c9adcd79d51da982a0599
jazmyn cortez <jazmyncortez@gmail.com>
2020-04-29 10:29:43 -0700

    you try now

--- arraysof-jazyjaz-austin.js
+++ arraysof-jazyjaz-austin.js
@@ -1,2 +1,7 @@
 // lets make some arrays
-var 
+//Consider this variable:
+//Write the code that will add "soda" to the end of the original array.
+//Write the code that will add "granola" to the end of array without altering the original array.
+var groceryList = ["chips", "dip", "cookies"]
+var groceryAdd = groceryList.push("soda")
+console.log(groceryList);
