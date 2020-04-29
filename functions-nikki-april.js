0470385e2098fda1bd03f1c63e496f5fbfa86a10
April Rieger <april.rieger@yahoo.com>
2020-04-29 14:52:53 -0700

    April's commit with challenges 3&4

--- functions-nikki-april.js
+++ functions-nikki-april.js
@@ -7,9 +7,40 @@
 
 //declare a function named greeting and it takes an argument name
 //var name = "NIKKI"
-const greeting = (name) => {
+// const greeting = (name) => {
+//
+//   //returns "Welcome, <person's name here>!"
+//   return `Welcome, ${name}!`
+// }
+// console.log(greeting("nikki"))
 
-  //returns "Welcome, <person's name here>!"
-  return `Welcome, ${name}!`
-}
-console.log(greeting("nikki"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+//declare a function named oddOrEven that takes an number as an argument
+// const oddOrEven = (number) => {
+// //check to see if the number is even by using % -- modulo if (12 % 2 === 0)
+//   if (number % 2 === 0){
+//     //if the number is even it should return as "The number is even"
+//     return "The number is even"
+//   } else {
+//   //if the number is not even then return "The number is odd"
+//     return "The number is odd"
+//   }
+// }
+// console.log(oddOrEven(24))
+// console.log(oddOrEven(23))
+// console.log(oddOrEven(224))
+// console.log(oddOrEven(254))
+// console.log(oddOrEven(54))
+// console.log(oddOrEven(664))
+
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+//declare a function names triple that takes a number as an arguement
+// const triple = (number) => {
+//   //returns the result of the number multiplied by 3
+//   return (number * 3)
+// }
+// console.log (triple(3))
