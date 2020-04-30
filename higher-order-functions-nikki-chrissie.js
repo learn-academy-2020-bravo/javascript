a8328a7da93807985b38ce740a14227f8317cd2c
LEARN Academy <cakau9@gmail.com>
2020-04-30 11:34:57 -0700

    Second Commit

--- higher-order-functions-nikki-chrissie.js
+++ higher-order-functions-nikki-chrissie.js
@@ -14,7 +14,16 @@ var arr2 = [2, 7, 3, 5, 8, 10, 13]
 //declare a function
 const oddNums = (array) => {
     return array.filter((value, index) => {
-        return index % 2 === 1
+        return value % 2 === 1
     })
 }
 console.log(oddNums(arr2))
+
+//Write a function that takes in an array of numbers and letters and returns a string with only the letters
+ var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+
+ //Declare a function that takes an array and return an array with only the letters
+
+ const letters = (array) => {
+    
+ }
