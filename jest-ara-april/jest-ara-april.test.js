90d8c1cf2f497f84a1840d570f17ff9d0276d148
April Rieger <april.rieger@yahoo.com>
2020-04-30 16:49:36 -0700

    aprils 2nd win with ara

--- jest-ara-april/jest-ara-april.test.js
+++ jest-ara-april/jest-ara-april.test.js
@@ -1,34 +1,55 @@
-describe("areYouTired", () => {
-  test("will return the statement drink coffee if you are tired and keep working if you are not tires",() => {
-    expect(areYouTired("yes")).toEqual("drink coffee")
-    expect(areYouTired("no")).toEqual("keep working")
-  })
-})
-
-const areYouTired = (string) => {
-  if(string === "yes"){
-    return "drink coffee"
-  } else {
-    return "keep working"
-  }
-}
+// describe("areYouTired", () => {
+//   test("will return the statement drink coffee if you are tired and keep working if you are not tires",() => {
+//     expect(areYouTired("yes")).toEqual("drink coffee")
+//     expect(areYouTired("no")).toEqual("keep working")
+//   })
+// })
+//
+// const areYouTired = (string) => {
+//   if(string === "yes"){
+//     return "drink coffee"
+//   } else {
+//     return "keep working"
+//   }
+// }
 
 // Write the test for a function that returns "relax" if you are stressed and "keep going" if you are not stressed. Write the function that will make the test pass.
 
 //describe a function that takes an argument
-describe("areYouStressed", () => {
-  test("will return relax if you are stressed or keep going if you are not stressed", () => {
-    expect(areYouStressed("yes")).toEqual("relax")
-    expect(areYouStressed("no")).toEqual("keep going")
-  })
-})
-
-const areYouStressed = (string) => {
-  if(string === "yes") {
-    return "relax"
-  } else {
-    return "keep going"
-  }
-}
+// describe("areYouStressed", () => {
+//   test("will return relax if you are stressed or keep going if you are not stressed", () => {
+//     expect(areYouStressed("yes")).toEqual("relax")
+//     expect(areYouStressed("no")).toEqual("keep going")
+//   })
+// })
+//
+// const areYouStressed = (string) => {
+//   if(string === "yes") {
+//     return "relax"
+//   } else {
+//     return "keep going"
+//   }
+// }
 //test what we expect be return
 //expect the outcome
+
+// Write the test for a function that returns "in budget" if a price is lower than $300. Write the function that will make the test pass.
+
+//describe a function that takes an argument
+describe("areYouInBudget", () => {
+  //test that the outcome is as expected
+  test("will return if you are in budget if the price is lower than $300", () => {
+    //expect the outcome
+    expect(areYouInBudget(value < 300)).toEqual("in budget")
+
+  })
+})
+ var value = Math.random()
+
+const areYouInBudget = (value) => {
+  if(value < 300) {
+    return "in budget"
+  } else {
+    return "you are not in budget"
+    }
+}
