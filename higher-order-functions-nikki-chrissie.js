ca9620a378c63fb379f010474539ddc252e17772
nbrooks89 <nbrooks2389@gmail.com>
2020-04-30 11:25:22 -0700

    first higher order

---
+++ higher-order-functions-nikki-chrissie.js
@@ -0,0 +1,20 @@
+
+
+var arr1 = [3, 9, 15, 4, 10]
+// declare a function that takes in an array and multiplies each index by 10
+const mult10 = (array) => {
+    //multiplies each index by 10
+    return array.map(value => value * 10)
+}
+// prints new array with variable multiplied by 10
+console.log(mult10(arr1))
+
+//Write a function that takes in an array and returns a new array with only odd numbers.
+var arr2 = [2, 7, 3, 5, 8, 10, 13]
+//declare a function
+const oddNums = (array) => {
+    return array.filter((value, index) => {
+        return index % 2 === 1
+    })
+}
+console.log(oddNums(arr2))
